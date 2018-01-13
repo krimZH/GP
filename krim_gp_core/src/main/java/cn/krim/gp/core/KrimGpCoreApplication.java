e49a3f79bb4ffef677fa810c6ecf976f26f1ac5a
krimZH <837656665@qq.com>
2018-01-13 09:54:02 +0800

    syc

--- krim_gp_core/src/main/java/cn/krim/gp/core/KrimGpCoreApplication.java
+++ krim_gp_core/src/main/java/cn/krim/gp/core/KrimGpCoreApplication.java
@@ -1,9 +1,11 @@
 package cn.krim.gp.core;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients(basePackages={"cn.krim.gp.core.coreController"})
+@SpringCloudApplication
 public class KrimGpCoreApplication {
 
 	public static void main(String[] args) {
